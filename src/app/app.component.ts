07eb4f02dde8ab0b6012e395ebece7a42a1faafe
Nathan R Smith <nrsmith012@gmail.com>
2017-09-21 14:48:15 -0700

    barcode gen working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,9 @@ export class AppComponent {
   emails: object[] = [{from: 'microsoft@bill.gates', priority: true, subject: 'upgrade your Windows to Doors', content: 'try my new OS, we are calling it Doors. much better than windows'},
   {from: 'chairman@board.borg', priority: true, subject: 'dirty joke', content: 'so dirty omg! lol okay... so, this horse, huh-huh, so he fell down in the mud!'},
   {from: 'lol@cats.lol', priority: false, subject: 'got milk?', content: "meow"},
-  {from: 'admin@broski.gov', priority: false, subject: 'invest in doors!', content: 'omg, M$ finally released my dream product: DOORS OS.... the first microsoft OS designed specifically for jailbroken iphones!!'}]
+  {from: 'admin@broski.gov', priority: false, subject: 'invest in doors!', content: 'omg, M$ finally released my dream product: DOORS OS.... the first microsoft OS designed specifically for jailbroken iphones!!'}];
 
-  colors: Array<string> = ['AliceBlue',
+  colors = ['AliceBlue',
   'AntiqueWhite',
   'Aqua',
   'Aquamarine',
@@ -161,8 +161,7 @@ export class AppComponent {
   'WhiteSmoke',
   'Yellow'];
 
-  getColor = function (){ 
-    return this.colors[Math.random() * (this.colors.length - 1)];
-  }
+  colorChoice = () => this.colors[Math.floor(Math.random() * this.colors.length)];
 
+  bars = Array.from(Array(12).keys());
 }
