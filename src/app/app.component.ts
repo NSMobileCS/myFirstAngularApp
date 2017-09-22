d6025d5a74df8abf878b65e4e0fa8ed25a7c9189
Nathan R Smith <nrsmith012@gmail.com>
2017-09-21 18:58:27 -0700

    minor bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  users = [];
+
+  user = {
+    name: '',
+    email: ''
+  };
+
+  updateMethod = () => {
+                        this.users.push(this.user); 
+                        this.user = {name:'', email:''};
+                       };
+
   emails: object[] = [{from: 'microsoft@bill.gates', priority: true, subject: 'upgrade your Windows to Doors', content: 'try my new OS, we are calling it Doors. much better than windows'},
   {from: 'chairman@board.borg', priority: true, subject: 'dirty joke', content: 'so dirty omg! lol okay... so, this horse, huh-huh, so he fell down in the mud!'},
   {from: 'lol@cats.lol', priority: false, subject: 'got milk?', content: "meow"},
